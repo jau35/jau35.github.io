10b3c788eda579f03f09f3943f10586c56d9b57c
jau35 <joeurbano14@gmail.com>
2019-11-04 20:52:59 -0500

    Updates

---
+++ precache-manifest.2e3cad4debedf942de03a483f3d6780d.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74094e249e02fbecb77d0a1dbc7fbe20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80a96fd1062f885ba274",
+    "url": "/static/css/main.bfab5585.chunk.css"
+  },
+  {
+    "revision": "d253806dd50b02f1342b",
+    "url": "/static/js/2.93c08c29.chunk.js"
+  },
+  {
+    "revision": "80a96fd1062f885ba274",
+    "url": "/static/js/main.dc65a28c.chunk.js"
+  },
+  {
+    "revision": "1bd8090ba4119beace63",
+    "url": "/static/js/runtime-main.f9abf3e9.js"
+  },
+  {
+    "revision": "03e91f122aa5fd425abbe23c85546eb0",
+    "url": "/static/media/Linearicons-Free.03e91f12.woff2"
+  },
+  {
+    "revision": "2781bf543c08d394dc78dec86989e3fd",
+    "url": "/static/media/Linearicons-Free.2781bf54.svg"
+  },
+  {
+    "revision": "2f3e9f80fff7d699dd3de6904d7d1647",
+    "url": "/static/media/Linearicons-Free.2f3e9f80.ttf"
+  },
+  {
+    "revision": "65060723fe964f85afa0a82d0bb78cf9",
+    "url": "/static/media/Linearicons-Free.65060723.woff"
+  },
+  {
+    "revision": "b9b7f23cb61b1f503e1249b63d980448",
+    "url": "/static/media/Linearicons-Free.b9b7f23c.eot"
+  },
+  {
+    "revision": "dfe2234b53e1c3446a98eedd9db89591",
+    "url": "/static/media/about-img.dfe2234b.jpg"
+  },
+  {
+    "revision": "89a2ee092e87759b6c08ddff670a1b74",
+    "url": "/static/media/banner-bg.89a2ee09.jpg"
+  },
+  {
+    "revision": "a7f2cf433fcbf8bf7d0fc8b92b16b8fb",
+    "url": "/static/media/banner-img.a7f2cf43.png"
+  },
+  {
+    "revision": "9f2045b40ed255448fdd2810bed50697",
+    "url": "/static/media/bentleysystems.9f2045b4.png"
+  },
+  {
+    "revision": "ae8efad423bc75b847fcad274f929988",
+    "url": "/static/media/blog-home-banner.ae8efad4.jpg"
+  },
+  {
+    "revision": "5383bba87b845e3d2fbfc4eb7a7ee8e2",
+    "url": "/static/media/cardconnect.5383bba8.png"
+  },
+  {
+    "revision": "69d5173e0707dee06cc534c82c1ed8c9",
+    "url": "/static/media/drexel.69d5173e.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "7fddc33bd65580e9d2bf4dc847bc19e5",
+    "url": "/static/media/siemens.7fddc33b.png"
+  }
+]);
